606b47cebcee0ad0a75c8bad592dd3295c1b0c35
lucbonar <guitarwar200@gmail.com>
2025-03-05 18:11:03 -0300

    Actualizando lista en pantalla

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
-let listaDeAmigos = [];
+let amigos = [];
 
 function validarEntrada() {
-    return (document.getElementById('amigo').value === '') ? true : false;
+    return (document.getElementById('amigo').value != '') ? true : false;
+}
+
+function actualizarLista(listaAmigos){
+    //obtengo el elemento lista
+    let lista = document.getElementById('listaAmigos');
+    //Limpio la lista existente
+    lista.innerHTML = '';
+    //Recorro y agrego elementos en la lista
+    listaAmigos.forEach(amigo => {
+        lista.innerHTML+=`<li>${amigo}</li>`;
+    });
+    return;
 }
 
 function agregarAmigo() {
     if (validarEntrada()) {
-        listaDeAmigos.push(document.getElementById('amigo').value);
-        console.log(listaDeAmigos);
-        document.getElementById('amigo').value = ''
+        //agregando nombres a la lista
+        amigos.push(document.getElementById('amigo').value);
+        //actualizar lista
+        actualizarLista(amigos);
+        //limpiando el input
+        document.getElementById('amigo').value = '';
     } else {
         alert('Por favor, inserte un nombre.')
     }
