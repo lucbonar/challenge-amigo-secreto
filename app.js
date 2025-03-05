5d74fb074c4eddc6c1f120ef9be53b665060c5c9
lucbonar <guitarwar200@gmail.com>
2025-03-05 17:17:49 -0300

    Validar y Agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaDeAmigos = [];
+
+function validarEntrada() {
+    return (document.getElementById('amigo').value === '') ? true : false;
+}
+
+function agregarAmigo() {
+    if (validarEntrada()) {
+        listaDeAmigos.push(document.getElementById('amigo').value);
+        console.log(listaDeAmigos);
+        document.getElementById('amigo').value = ''
+    } else {
+        alert('Por favor, inserte un nombre.')
+    }
+    return;
+}
+
+function sortearAmigo() {
+    return;
+}
